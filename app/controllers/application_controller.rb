ed96d784f5e19c5efde063beff77c19d0b4ffdd6
gokart <sergykoretsfsp@gmail.com>
2016-12-20 22:43:17 +0200

    heroku config3

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -26,7 +26,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :password])
-    devise_parameter_sanitizer.permit(:account_update, [:name, :email, :password, :current_password, :avatar]) 
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :email, :password, :current_password, :avatar]) 
   end
 
 end
